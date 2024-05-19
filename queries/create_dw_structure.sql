2bc823b5aed47fbd2a8744c9625ef892b7baf30c
lucas.alves <lucas.alves@ids.inf.br>
2024-05-19 18:30:24 -0300

    update create dw tables

--- queries/create_dw_structure.sql
+++ queries/create_dw_structure.sql
@@ -1,16 +1,10 @@
-DROP SCHEMA IF EXISTS rating_analysis CASCADE;
-
-CREATE SCHEMA rating_analysis;
-
-DROP TABLE IF EXISTS time;
-CREATE TABLE time (
+CREATE TABLE dates (
     date DATE NOT NULL PRIMARY KEY,
-    day INT NOT NULL CHECK(day>=1 AND day<=31),
-    month INT NOT NULL CHECK(month>=1 AND month<=12),
+    day INT NOT NULL,
+    month INT NOT NULL,
     year INT NOT NULL
 );
 
-DROP TABLE IF EXISTS movies;
 CREATE TABLE movies(
     id VARCHAR(100) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -18,12 +12,11 @@ CREATE TABLE movies(
     hosted_at VARCHAR(100) NOT NULL
 );
 
-DROP TABLE IF EXISTS rating;
 CREATE TABLE rating(
     rating_date DATE NOT NULL,
     client_id VARCHAR(100) NOT NULL,
     movie_id VARCHAR(100) NOT NULL,
     rating FLOAT,
-    FOREIGN KEY (rating_date) REFERENCES time(date),
+    FOREIGN KEY (rating_date) REFERENCES dates(date),
     FOREIGN KEY (movie_id) REFERENCES movies(id)
 );
